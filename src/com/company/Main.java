9b43b0ac9b11cdd46288c0995009272f50386a44
Marwa Mubarak <marwamubarak6@gmail.com>
2022-12-19 05:16:37 +0200

    finish

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,185 +1,89 @@
 package com.company;
 
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Scanner;
-import java.util.Vector;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.*;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
-
-    static Vector<CodeBook> codeBooks =new Vector<CodeBook>();
-    static Vector<CodeBook> allCodeBooks =new Vector<CodeBook>();
-
-    public static  int[][] getAverage(Vector<CodeBook> allCodeBooks) {
-        int vectorSize = allCodeBooks.get(0).vector.length;
-        int[][] average = new int[vectorSize][vectorSize];
-        for (int i = 0; i < vectorSize; i++)
-            for (int j = 0; j < vectorSize; j++)
-                average[i][j] = 0;
-        for (int k = 0; k < allCodeBooks.size(); k++)
-            for (int i = 0; i < vectorSize; i++)
-                for (int j = 0; j < vectorSize; j++)
-                    average[i][j] += allCodeBooks.get(k).vector[i][j];
-        for (int i = 0; i < vectorSize; i++)
-            for (int j = 0; j < vectorSize; j++)
-                average[i][j] /= allCodeBooks.size();
-        return average;
-    }
-    public static  int getIdx(CodeBook codeBook, Vector<Vector<CodeBook>>spiltTree) {
-        int idx = -1;
-        for (int i = 0; i < spiltTree.size(); i++)
-        {
-            for (int j = 0; j < spiltTree.get(i).size(); j++) {
-                if (spiltTree.get(i).get(j).vector == codeBook.vector)
-                    idx = i;
-            }
-        }
-        return idx;
-    }
-    public static  int LeftOrRight(CodeBook codeBook, Vector<Vector<CodeBook>>spiltTree) { //compare
-        int idx = -1,mn =Integer.MAX_VALUE,diff=0;
-        for(int k=0;k<spiltTree.size();k++) {
-            diff=0;
-            for (int i = 0; i < codeBook.vector.length; i++) {
-                for (int j = 0; j < codeBook.vector.length; j++) {
-                    diff += Math.abs(codeBook.vector[i][j]-(spiltTree.get(k).get(0).vector[i][j]));
-                }
-            }
-            if(diff<mn)
-            {
-                mn = diff;
-                idx = k;
-                //diff = 0;
-            }
-        }
-        return idx;
-    }
-    public static void Compression(int numOfCodeBook) throws IOException {
-
-       int n=(int)Math.sqrt(allCodeBooks.size());
-       Vector<Vector<CodeBook>> spiltTree=new Vector<>();
-       int [][] averageCodeBooks=getAverage(allCodeBooks);
-       int vectorSize =allCodeBooks.get(0).vector.length;
-       int leftAverage[][]=new int[vectorSize][vectorSize];
-       int rightAverage[][]=new int[vectorSize][vectorSize];
-        for (int i = 0; i < vectorSize; i++) {
-            for (int j = 0; j < vectorSize; j++) {
-                leftAverage[i][j]=averageCodeBooks[i][j]-1;
-                rightAverage[i][j]=averageCodeBooks[i][j]+1;
-            }
-        }
-        Vector<CodeBook>rightCode=new Vector<>();
-        rightCode.add(new CodeBook(rightAverage));
-        Vector<CodeBook>leftCode=new Vector<>();
-        leftCode.add(new CodeBook(leftAverage));
-        spiltTree.add(leftCode);
-        spiltTree.add(rightCode);
-        int f=0;
-        int spilt=(int)(Math.log10(numOfCodeBook)/Math.log10(2));
-        for (int i = 0; i < spilt; i++) {
-            for (int j = 0; j < allCodeBooks.size(); j++) {
-                int idx=LeftOrRight(allCodeBooks.get(j),spiltTree);
-                spiltTree.get(idx).add(allCodeBooks.get(j));
-            }
-            Vector<CodeBook>midPoints=new Vector<>();
-            for (int j = 0; j < spiltTree.size(); j++) {
-                averageCodeBooks=getAverage(spiltTree.get(j));
-                midPoints.add(new CodeBook(averageCodeBooks));
-                // update the average point in the tree
-                spiltTree.get(j).get(0).vector=averageCodeBooks;
-
-            }
-
-            if(i!=spilt-1){
-                spiltTree.clear();
-                for (int j = 0; j < midPoints.size(); j++) {
-                    for (int k = 0; k < vectorSize; k++) {
-                        for (int l = 0; l < vectorSize; l++) {
-                            leftAverage[k][l] = midPoints.elementAt(j).vector[k][l] - 1;
-                            rightAverage[k][l] = midPoints.elementAt(j).vector[k][l] + 1;
-
-                        }
-                    }
-                    rightCode.clear();
-                    rightCode.add(new CodeBook(rightAverage));
-                    leftCode.clear();
-                    leftCode.add(new CodeBook(leftAverage));
-                    spiltTree.add(leftCode);
-                    spiltTree.add(rightCode);
-                }
-            }else if(f==0){
-                spilt++;
-                f=1;
-
-            }
-
-
-        }
-        for (int i = 0; i < spiltTree.size(); i++) {
-            CodeBook codeBook=spiltTree.get(i).get(0);
-            codeBook.code=i;
-            codeBooks.add(codeBook);
-        }
-        //compress the image
-        int compressedCodeBooks[][]=new int[n][n];
-        for (int i = 0; i < compressedCodeBooks.length; i++) {
-            for (int j = 0; j < compressedCodeBooks.length; j++) {
-                compressedCodeBooks[i][j] = getIdx(allCodeBooks.get(j), spiltTree);
-            }
-        }
-
-        // write image at the file
-        FileWriter fileWriter= null;
-        try {
-            fileWriter = new FileWriter("x.txt");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        for (int i = 0; i <compressedCodeBooks.length ; i++) {
-            for (int j = 0; j < compressedCodeBooks.length; j++) {
-                fileWriter.write(compressedCodeBooks[i][j]+" ");
-            }
-            fileWriter.write("\n");
-        }
-        fileWriter.close();
-
-
-
-
-
-
-    }
-
-    public static void Decompression(){
-
-    }
-    public static void divideImage(int pixels[][], int vectorSize){
-       int n= pixels.length/vectorSize;
-        for (int i = 0; i <= n*vectorSize; i+=vectorSize) {
-            for (int j = 0; j < n*vectorSize; j+=vectorSize) {
-                int vector[][] =new int[vectorSize][vectorSize];
-                for (int k = i,x=0; k < vectorSize+i;x++, k++) {
-                    for (int l = j,y=0; l < vectorSize+j;y++, l++) {
-                        vector[x][y]=pixels[i][j];
-                    }
-                }
-                CodeBook codeBook=new CodeBook(vector);
-                allCodeBooks.add(codeBook);
-
-            }
-        }
-
-    }
-
     public static Scanner input =new Scanner(System.in);
-    public static void main(String[] args) throws IOException {
-        int vectorSize=input.nextInt();
-        int numOfCodeBook=input.nextInt();
-        ReadWriteImage readWriteImage=new ReadWriteImage();
-        int pixels[][]=readWriteImage.readImage("img.jpg");
-        divideImage(pixels,vectorSize);
-        Compression(numOfCodeBook);
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+
+        JFrame frame_ = new JFrame("Image Compress App");
+        JLabel label_1, label_2 ,label_3;
+
+        label_1 = new JLabel("Enter Vector Height");
+        label_1.setBounds(50, 20, 300, 25);
+        final JTextField input1 = new JTextField();
+        input1.setBounds(190, 25, 150, 25);
+
+        label_2 = new JLabel("Enter Vector Width");
+        label_2.setBounds(50, 70, 300, 25);
+        final JTextField input2 = new JTextField();
+        input2.setBounds(190, 70, 150, 25);
+
+        label_3 = new JLabel("Enter Vector Width");
+        label_3.setBounds(50, 120, 300, 25);
+        final JTextField input3 = new JTextField();
+        input3.setBounds(190, 120, 150, 25);
+
+        JButton button1 = new JButton("Compression");
+        button1.setBounds(350, 150, 150, 30);
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+//
+//        einstein.jpg
+//        image.jpg
+//        img.jpg
+                try {
+                    VectorQuantization.originalImage = VectorQuantization.readImage("img.jpg");
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+                VectorQuantization.vectorWidth = Integer.parseInt(input2.getText());
+                VectorQuantization.vectorHeight = Integer.parseInt(input1.getText());
+                VectorQuantization.numOfCodebook = Integer.parseInt(input3.getText());
+                VectorQuantization.Compression();
+                VectorQuantization.writeToFile("x.txt");
+                try {
+                    VectorQuantization.readFromFile("x.txt");
+                } catch (FileNotFoundException ex) {
+                    ex.printStackTrace();
+                }
+                VectorQuantization.Decompression();
+                VectorQuantization.writeImage(VectorQuantization.collectedImage, "out.jpg");
+
+                try {
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+                System.exit(0);
+
+
+            }
+        });
+
+        frame_.add(label_1);
+        frame_.add(label_2);
+        frame_.add(label_3);
+        frame_.add(button1);
+        frame_.add(input1);
+        frame_.add(input2);
+        frame_.add(input3);
+        frame_.setSize(600, 250);
+        frame_.setLayout(null);
+        frame_.setVisible(true);
+
+
+
+
+
+
 
     }
 }
---
+++ src/com/company/VectorQuantization.java
@@ -0,0 +1,362 @@
+package com.company;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.util.*;
+
+public class VectorQuantization {
+    public static int imageHeight, imageWidth, vectorHeight, vectorWidth, numOfCodebook, compressedHeight, compressedWidth;
+    // the original image
+    public static float[][] originalImage;
+    // the compressed image
+    public static String[][] compressedImage;
+    // the collected image
+    public static float[][] collectedImage;
+    // the original blocks of the image
+    public static ArrayList<float[][]> originalBlocks = new ArrayList<>();
+    // the blocks of the image after operations(spilt&average)
+    public static ArrayList<float[][]> blocks = new ArrayList<>();
+    // the vector nearest to what of the original vectors of the image
+    public static Map<float[][], ArrayList<float[][]>> nearestVectors = new HashMap<>();
+    // the codebook
+    public static TreeMap<String, float[][]> codeBook = new TreeMap<>();
+
+    // to read the image
+    public static float[][] readImage(String filePath) throws IOException {
+        File file = new File(filePath);
+        BufferedImage image;
+        image = ImageIO.read(file);
+        imageWidth = image.getWidth();
+        imageHeight = image.getHeight();
+        float[][] pixels = new float[imageHeight][imageWidth];
+        int rgb;
+        for (int i = 0; i < imageWidth; i++) {
+            for (int j = 0; j < imageHeight; j++) {
+                rgb = image.getRGB(i, j);
+                int red = (rgb & 0x00ff0000) >> 16;
+                int green = (rgb & 0x0000ff00) >> 8;
+                int blue = (rgb & 0x000000ff);
+                pixels[j][i] = Math.max(Math.max(red, green), blue);
+            }
+        }
+        return pixels;
+    }
+
+    // to write the image
+    public static void writeImage(float[][] pixels, String filePath) {
+        File fileout = new File(filePath);
+        int w, h;
+        h = pixels.length;
+        w = pixels[0].length;
+        BufferedImage image2 = new BufferedImage(w, h, BufferedImage.TYPE_3BYTE_BGR);
+
+        for (int x = 0; x < h; x++)
+            for (int y = 0; y < w; y++) {
+                int value = 0xff000000 | ((int) pixels[x][y] << 16) | ((int) pixels[x][y] << 8) | (int) pixels[x][y];
+                image2.setRGB(y, x, value);
+            }
+        try {
+            ImageIO.write(image2, "jpg", fileout);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static void divideImage() {
+        imageHeight = originalImage.length;
+        imageWidth = originalImage[0].length;
+        // to know if the vector w&h if dividable by image h&w
+        int h = imageHeight % vectorHeight;
+        int w = imageWidth % vectorWidth;
+        // this condition to resize the original image with the new size
+        // after removing the non-dividable part of the image
+
+        if ((h != 0) || (w != 0)) {
+            imageHeight -= h;
+            imageWidth -= w;
+            float[][] newImage = new float[imageHeight][imageWidth];
+            for (int i = 0; i < imageHeight; i++)
+                for (int j = 0; j < imageWidth; j++)
+                    newImage[i][j] = originalImage[i][j];
+            // resize the image with the new size
+            originalImage = newImage;
+        }
+
+        // loop overall the image matrix and put all the vector size with the image grb color then add it to the original blocks
+        for (int i = 0; i < imageHeight; i += vectorHeight) {
+            for (int j = 0; j < imageWidth; j += vectorWidth) {
+                float[][] vector = new float[vectorHeight][vectorWidth];
+                h = i;
+                for (int x = 0; x < vectorHeight; x++, h++) {
+                    w = j;
+                    for (int y = 0; y < vectorWidth; y++, w++) {
+                        vector[x][y] = originalImage[h][w];
+                    }
+                }
+                originalBlocks.add(vector);
+            }
+        }
+    }
+
+    //get the average of the vectors of the image and return the vector of it
+    public static float[][] getAverage(ArrayList<float[][]> vecs) {
+        float[][] average = new float[vectorHeight][vectorWidth];
+        for (int i = 0; i < vectorHeight; i++)
+            for (int j = 0; j < vectorWidth; j++) {
+                average[i][j] = 0;
+                for (int x = 0; x < vecs.size(); x++)
+                    average[i][j] += vecs.get(x)[i][j];
+                average[i][j] /= (float) vecs.size();
+            }
+        return average;
+    }
+
+    // split each vector to 2 vectors (up&down)
+    public static void splitVector() {
+        int size = blocks.size();
+        // for each block divide it into up and down vector and put it into the blocks
+        for (int i = 0; i < size; i++) {
+            float[][] down = new float[vectorHeight][vectorWidth];
+            float[][] up = new float[vectorHeight][vectorWidth];
+            for (int x = 0; x < vectorHeight; x++) {
+                for (int y = 0; y < vectorWidth; y++) {
+                    float d = (float) Math.floor(blocks.get(i)[x][y]);
+                    float u = (float) Math.ceil(blocks.get(i)[x][y]);
+                    if (d == u) {
+                        d--;
+                        u++;
+                    }
+                    down[x][y] = d;
+                    up[x][y] = u;
+                }
+            }
+            blocks.add(down);
+            blocks.add(up);
+        }
+        // to remove the old blocks of the array and let the new in it
+        while (size != 0) {
+            blocks.remove(0);
+            size--;
+        }
+    }
+
+
+    public static void getNearestVectorsOfTheOriginalImage() {
+        // for each original book find the smallest difference between it and all the blocks by get the abs between them
+        for (int i = 0; i < originalBlocks.size(); i++) {
+            ArrayList<Double> differences = new ArrayList<>();
+            for (int j = 0; j < blocks.size(); j++) {
+                double difference = 0;
+                for (int x = 0; x < vectorHeight; x++) {
+                    for (int y = 0; y < vectorWidth; y++) {
+                        difference += Math.pow(((double) blocks.get(j)[x][y] - (double) originalBlocks.get(i)[x][y]), 2);
+                    }
+                }
+                differences.add(difference);
+            }
+            // get the min value the get its index
+            double min = Collections.min(differences);
+            int index = differences.indexOf(min);
+            //then put the original block at the nearest vector of it
+            if (nearestVectors.containsKey(blocks.get(index)))
+                nearestVectors.get(blocks.get(index)).add(originalBlocks.get(i));
+            else {
+                ArrayList<float[][]> nearestVectorsOfThisBlock = new ArrayList<>();
+                nearestVectorsOfThisBlock.add(originalBlocks.get(i));
+                nearestVectors.put(blocks.get(index), nearestVectorsOfThisBlock);
+            }
+        }
+    }
+
+
+    public static void Compression() {
+        divideImage();// divide the image into vectors and put them into the original image
+        blocks.add(getAverage(originalBlocks));// get the average of the original image to split the image into to sides
+        nearestVectors.put(blocks.get(0), originalBlocks);// the block that represent the whole of the image
+        // split until find the number of codebook that we need
+        while (blocks.size() < numOfCodebook) {
+            splitVector();
+            nearestVectors.clear();// clear the nearest old vectors
+            getNearestVectorsOfTheOriginalImage();// get the nearest new vectors
+            blocks.clear();// clear the old blocks
+            // get the average of the new blocks(the nearest vectors) and put them in the block array
+            for (float[][] vec : nearestVectors.keySet()) {
+                float[][] average = getAverage(nearestVectors.get(vec));
+                blocks.add(average);
+            }
+        }
+        //while true until the vectors be the same
+        while (true) {
+            int counter = 0;
+            for (int i = 0; i < blocks.size(); i++) {
+                float[][] vec = blocks.get(i);
+                for (float[][] vector : nearestVectors.keySet()) {
+                    int c = 0;
+                    for (int x = 0; x < vectorHeight; x++) {
+                        for (int y = 0; y < vectorWidth; y++) {
+                            if (vector[x][y] == vec[x][y])
+                                c++;
+                        }
+                    }
+                    // the two vectors are equal
+                    if (c == (vectorHeight * vectorWidth))
+                        counter++;
+                }
+            }
+
+            if (counter == blocks.size())// they are the same so finish
+                break;
+            // else -> repeat.. get the nearest vectors and the blocks average then check again
+            nearestVectors.clear();
+            getNearestVectorsOfTheOriginalImage();
+            blocks.clear();
+            for (float[][] vec : nearestVectors.keySet())
+                blocks.add(getAverage(nearestVectors.get(vec)));
+        }
+        generateCompressedCode();
+    }
+
+
+    public static void generateCompressedCode() {
+        // get the number of bits to write the encoded code
+        // log2(numOfCodeBook)
+        int nBits = (int) Math.ceil((Math.log(numOfCodebook) / Math.log(2)));
+
+        for (int i = 0; i < blocks.size(); i++) {
+            // convert int to binary
+            String binary = Integer.toBinaryString(i);
+            // put zeros to left if it's written in bits less than nBits
+            while (binary.length() != nBits)
+                binary = "0" + binary;
+            // put it at the codebook
+            codeBook.put(binary, blocks.get(i));
+        }
+
+        compressedHeight = imageHeight / vectorHeight;
+        compressedWidth = imageWidth / vectorWidth;
+
+        compressedImage = new String[compressedHeight][compressedWidth];
+        int index = 0;
+        //for each cell of the compressed image put the code of it
+        for (int i = 0; i < compressedHeight; i++) {
+            for (int j = 0; j < compressedWidth; j++) {
+                for (float[][] vec : nearestVectors.keySet()) {
+                    if (nearestVectors.get(vec).contains(originalBlocks.get(index))) {
+                        compressedImage[i][j] = getCode(vec);
+                    }
+                }
+                index++;
+            }
+        }
+    }
+
+    // get the code of the compressed image vector
+    public static String getCode(float[][] vector) {
+        for (String code : codeBook.keySet()) {
+            int counter = 0;
+            for (int i = 0; i < vectorHeight; i++) {
+                for (int j = 0; j < vectorWidth; j++) {
+                    if (vector[i][j] == codeBook.get(code)[i][j])
+                        counter++;
+                }
+            }
+            if (counter == (vectorHeight * vectorWidth))
+                return code;
+        }
+        return null;
+    }
+
+    public static void Decompression() {
+        for (String code : codeBook.keySet()) {
+            vectorHeight = codeBook.get(code).length;
+            vectorWidth = codeBook.get(code)[0].length;
+            break;
+        }
+        imageHeight = compressedHeight * vectorHeight;
+        imageWidth = compressedWidth * vectorWidth;
+        collectedImage = new float[imageHeight][imageWidth];
+        int h, w;
+        for (int i = 0, a = 0; i < imageHeight; i += vectorHeight, a++) {
+            for (int j = 0, b = 0; j < imageWidth; j += vectorWidth, b++) {
+                h = i;
+                float[][] vector = codeBook.get(compressedImage[a][b]);
+                for (int x = 0; x < vectorHeight; x++, h++) {
+                    w = j;
+                    for (int y = 0; y < vectorWidth; y++, w++) {
+                        collectedImage[h][w] = vector[x][y];
+                    }
+                }
+            }
+        }
+    }
+
+    // the following to write the compressed code of the image and the codebook at file
+    public static void writeToFile(String fileName) {
+        String image = "";
+        for (int i = 0; i < compressedHeight; i++) {
+            for (int j = 0; j < compressedWidth; j++)
+                image += compressedImage[i][j] + " ";
+            image += "\n";
+        }
+
+        image = image.substring(0, image.length() - 1);
+        for (String code : codeBook.keySet()) {
+            image += code + "\n";
+            String temp = code;
+            temp.replaceAll("0", "1");
+            for (int i = 0; i < vectorHeight; i++) {
+                for (int j = 0; j < vectorWidth; j++) {
+                    image += codeBook.get(code)[i][j] + " ";
+                }
+                image += "\n";
+            }
+        }
+
+        image = image.substring(0, image.length() - 2);
+
+        File file = new File(fileName);
+        try {
+            file.createNewFile();
+            BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+            writer.write(image);
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // to reconstruct the compressed image and the codebook from the file
+    public static void readFromFile(String fileName) throws FileNotFoundException {
+        compressedImage = new String[compressedHeight][compressedWidth];
+        codeBook.clear();
+        File file = new File(fileName);
+        if (file.exists()) {
+            Scanner scanner = new Scanner(file);
+            for (int i = 0; i < compressedHeight; i++) {
+                for (int j = 0; j < compressedWidth; j++) {
+                    compressedImage[i][j] = scanner.next();
+                }
+            }
+            while (scanner.hasNextLine()) {
+                String code = scanner.next();
+
+                try {
+                    float[][] vector = new float[vectorHeight][vectorWidth];
+                    for (int i = 0; i < vectorHeight; i++) {
+                        for (int j = 0; j < vectorWidth; j++) {
+                            vector[i][j] = Float.parseFloat(scanner.next());
+                        }
+                    }
+                    codeBook.put(code, vector);
+                } catch (Exception e) {
+                    break;
+                }
+            }
+        } else {
+            System.out.println("File not found");
+        }
+    }
+
+}
